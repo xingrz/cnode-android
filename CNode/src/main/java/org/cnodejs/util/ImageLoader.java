8d3ab61991c75632069c7302a7681e7a80cab254
XiNGRZ <chenxingyu92@gmail.com>
2014-10-26 14:16:05 +0800

    改善 Gravatar 头像的清晰度

--- CNode/src/main/java/org/cnodejs/util/ImageLoader.java
+++ CNode/src/main/java/org/cnodejs/util/ImageLoader.java
@@ -13,6 +13,12 @@ public class ImageLoader {
             url = "https:" + url;
         }
 
+        // TODO: 暂时的 workaround， 未做仔细处理
+        // 将 Gravatar 默认的 48x48 替换成 120x120（按我们的 ImageView 大小是 40dp，xxhdpi 分辨率计算）
+        if (url.contains("gravatar.com") && url.contains("size=48")) {
+            url = url.replace("size=48", "size=120");
+        }
+
         Picasso picasso = Picasso.with(view.getContext());
         picasso.setLoggingEnabled(BuildConfig.DEBUG);
         picasso.load(url).into(view);
